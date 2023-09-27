2f16df0e0fea42abb3d510f9f6dc5144d299a243
nmhien2605 <nmhien2605@gmail.com>
2023-09-27 21:12:04 +0700

    repick colors

--- src/presetPalette/index.ts
+++ src/presetPalette/index.ts
@@ -1,28 +1,48 @@
 import { generatePalette } from "../generatePalette";
+import { mixColor } from "../mixColor";
 
 interface Palette {
   red: string[];
+  lava: string[];
   orange: string[];
+  amber: string[];
   yellow: string[];
+  lime: string[];
   green: string[];
+  cyan: string[];
   blue: string[];
+  indigo: string[];
   purple: string[];
+  magenta: string[];
+  gray: string[];
 }
 
-export const presetPalette: Palette = {
-  red: generatePalette("#FF4241"),
-  orange: generatePalette("#FF9341"),
-  yellow: generatePalette("#FFE541"),
-  green: generatePalette("#4EFF41"),
-  blue: generatePalette("#41ADFF"),
-  purple: generatePalette("#7841FF"),
-};
+const grayPalette: string[] = [
+  "#FFFFFF",
+  mixColor("#FFFFFF", "#000000", 0.9),
+  mixColor("#FFFFFF", "#000000", 0.8),
+  mixColor("#FFFFFF", "#000000", 0.7),
+  mixColor("#FFFFFF", "#000000", 0.6),
+  mixColor("#FFFFFF", "#000000", 0.5),
+  mixColor("#FFFFFF", "#000000", 0.4),
+  mixColor("#FFFFFF", "#000000", 0.3),
+  mixColor("#FFFFFF", "#000000", 0.2),
+  mixColor("#FFFFFF", "#000000", 0.1),
+  "#000000",
+];
 
-export const wdPresetPalette: Palette = {
-  red: generatePalette("#E81123"),
-  orange: generatePalette("#F7630C"),
-  yellow: generatePalette("#FFB900"),
-  green: generatePalette("#107C10"),
-  blue: generatePalette("#0078D7"),
-  purple: generatePalette("#9A0089"),
+export const presetPalette: Palette = {
+  red: generatePalette("#FF414E"),
+  lava: generatePalette(mixColor("#FF414E", "#FF9341", 0.5)),
+  orange: generatePalette("#FF9341"),
+  amber: generatePalette(mixColor("#FF9341", "#FFF241", 0.5)),
+  yellow: generatePalette("#FFF241"),
+  lime: generatePalette(mixColor("#FFF241", "#41FF5D", 0.5)),
+  green: generatePalette("#41FF5D"),
+  cyan: generatePalette(mixColor("#41FF5D", "#41ADFF", 0.5)),
+  blue: generatePalette("#41ADFF"),
+  indigo: generatePalette(mixColor("#41ADFF", "#9341FF", 0.5)),
+  purple: generatePalette("#9341FF"),
+  magenta: generatePalette(mixColor("#9341FF", "#FF414E", 0.5)),
+  gray: grayPalette,
 };
