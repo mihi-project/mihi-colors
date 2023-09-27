73f0d7d779fbab580416f45d686f6b6d13819399
nmhien2605 <nmhien2605@gmail.com>
2023-09-27 23:12:33 +0700

    update export value

--- src/presetPalette/index.ts
+++ src/presetPalette/index.ts
@@ -1,23 +1,19 @@
 import { generatePalette } from "../generatePalette";
 import { mixColor } from "../mixColor";
 
-interface Palette {
-  red: string[];
-  lava: string[];
-  orange: string[];
-  amber: string[];
-  yellow: string[];
-  lime: string[];
-  green: string[];
-  cyan: string[];
-  blue: string[];
-  indigo: string[];
-  purple: string[];
-  magenta: string[];
-  gray: string[];
-}
-
-const grayPalette: string[] = [
+const red: string[] = generatePalette("#FF414E");
+const lava: string[] = generatePalette(mixColor("#FF414E", "#FF9341", 0.5));
+const orange: string[] = generatePalette("#FF9341");
+const amber: string[] = generatePalette(mixColor("#FF9341", "#FFF241", 0.5));
+const yellow: string[] = generatePalette("#FFF241");
+const lime: string[] = generatePalette(mixColor("#FFF241", "#41FF5D", 0.5));
+const green: string[] = generatePalette("#41FF5D");
+const cyan: string[] = generatePalette(mixColor("#41FF5D", "#41ADFF", 0.5));
+const blue: string[] = generatePalette("#41ADFF");
+const indigo: string[] = generatePalette(mixColor("#41ADFF", "#9341FF", 0.5));
+const purple: string[] = generatePalette("#9341FF");
+const magenta: string[] = generatePalette(mixColor("#9341FF", "#FF414E", 0.5));
+const gray: string[] = [
   "#FFFFFF",
   mixColor("#FFFFFF", "#000000", 0.9),
   mixColor("#FFFFFF", "#000000", 0.8),
@@ -31,18 +27,34 @@ const grayPalette: string[] = [
   "#000000",
 ];
 
-export const presetPalette: Palette = {
-  red: generatePalette("#FF414E"),
-  lava: generatePalette(mixColor("#FF414E", "#FF9341", 0.5)),
-  orange: generatePalette("#FF9341"),
-  amber: generatePalette(mixColor("#FF9341", "#FFF241", 0.5)),
-  yellow: generatePalette("#FFF241"),
-  lime: generatePalette(mixColor("#FFF241", "#41FF5D", 0.5)),
-  green: generatePalette("#41FF5D"),
-  cyan: generatePalette(mixColor("#41FF5D", "#41ADFF", 0.5)),
-  blue: generatePalette("#41ADFF"),
-  indigo: generatePalette(mixColor("#41ADFF", "#9341FF", 0.5)),
-  purple: generatePalette("#9341FF"),
-  magenta: generatePalette(mixColor("#9341FF", "#FF414E", 0.5)),
-  gray: grayPalette,
+const presetPalette: Record<string, string[]> = {
+  red,
+  lava,
+  orange,
+  amber,
+  yellow,
+  lime,
+  green,
+  cyan,
+  blue,
+  indigo,
+  purple,
+  magenta,
+};
+
+export {
+  presetPalette,
+  red,
+  lava,
+  orange,
+  amber,
+  yellow,
+  lime,
+  green,
+  cyan,
+  blue,
+  indigo,
+  purple,
+  magenta,
+  gray,
 };
